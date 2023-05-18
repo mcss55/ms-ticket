db36ed02857cd0ee620f97c2249dd7c2c3cc4c5c
mcss55 <maqsudsafin@gmail.com>
2023-05-18 17:51:59 +0400

    ms-latest

---
+++ src/main/java/com/mcss/msticket/MsTicketApplication.java
@@ -0,0 +1,13 @@
+package com.mcss.msticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTicketApplication.class, args);
+    }
+
+}
